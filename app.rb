695b3f91fc1d04f01c765edb5ed79177d433e716
Gaizka <gaizkav@gmail.com>
2013-09-26 13:51:04 +0200

    Protect app with http basic auth

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'json'
 
 configure do
+  set :root, File.dirname(__FILE__)
   set :public_folder, Proc.new { File.join(root, "static") }
   DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/webhooks')
 end
@@ -11,13 +12,29 @@ end
 require './deployment'
 require './alert'
 
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    auth.provided? and auth.basic? and auth.credentials and auth.credentials ==
+      [ENV['HTTP_AUTH_USER'] || 'fooooo', ENV["HTTP_AUTH_PASSWORD"] || 'baaaaaar']
+  end
+end
+
 get '/' do
+  protected!
   @deployments = Deployment.order(:created_at.desc)
   @alerts = Alert.order(:created_at.desc)
   erb :index
 end
 
 post '/webhook' do
+  protected!
   if params[:deployment]
     deployment = Deployment.new(JSON.parse(params[:deployment]))
     deployment.save
